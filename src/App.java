69276095329b6ed5e3eb3e3db7048847fc497449
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-08-28 16:57:18 -0400

    improved code readability

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
 
 	public static void main(String[] args) {
 		Gui gui = new Gui();
-		gui.getGreetings();
-		gui.userInputField();
+		gui.setNorthPanel();
+		gui.setSouthPanel();
 		gui.generateGui();
 	}
 
--- src/Gui.java
+++ src/Gui.java
@@ -1,10 +1,16 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.net.URI;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -18,13 +24,14 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Gui implements ActionListener {
+public class Gui {
 
     private JFrame frame = new JFrame("Weather App");
-    private JPanel greetings = new JPanel();
-    private JPanel searchResult = new JPanel();
+    private JPanel northPanel = new JPanel();
+    private JPanel centerPanel = new JPanel();
     private JPanel details = new JPanel();
     private JPanel imagePanel = new JPanel();
+    private JPanel southPanel = new JPanel();
     private JTextField userInputField;
     private Transcript transcript;
     private final String DEGREE = "\u00B0";
@@ -37,7 +44,25 @@ public class Gui implements ActionListener {
         this.transcript = transcript;
     }
 
-    public void userInputField() {
+    public void setNorthPanel() {
+        northPanel.setLayout(new BoxLayout(northPanel, BoxLayout.Y_AXIS));
+        northPanel.setBorder(new EmptyBorder(0, 10, 0, 10));
+        northPanel.add(Box.createVerticalGlue());
+
+        addWelcomeLabel();
+        addSearchInputPanel();
+        addSearchButtonPanel();
+
+        frame.add(northPanel, BorderLayout.NORTH);
+    }
+
+    private void addWelcomeLabel() {
+        JLabel welcome = new JLabel("Welcome to Weather App");
+        welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
+        northPanel.add(welcome);
+    }
+
+    private void addSearchInputPanel() {
         JPanel searchInputPanel = new JPanel();
         searchInputPanel.setLayout(new BoxLayout(searchInputPanel, BoxLayout.X_AXIS));
         searchInputPanel.setBorder(new EmptyBorder(10, 0, 0, 0));
@@ -48,31 +73,183 @@ public class Gui implements ActionListener {
         userInputField = new JTextField(20);
         searchInputPanel.add(userInputField);
 
+        northPanel.add(searchInputPanel);
+    }
+
+    private void addSearchButtonPanel() {
         JPanel searchButtonPanel = new JPanel();
         searchButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         JButton searchWeather = new JButton("Search");
-        searchWeather.addActionListener(this);
+        searchWeather.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleSearchButtonClicked();
+            }
+        });
         searchButtonPanel.add(searchWeather);
-
-        greetings.add(searchInputPanel);
-        greetings.add(searchButtonPanel);
+        northPanel.add(searchButtonPanel);
     }
 
-    public void getGreetings() {
-        greetings.setLayout(new BoxLayout(greetings, BoxLayout.Y_AXIS));
-        greetings.setBorder(new EmptyBorder(0, 10, 0, 10));
-        greetings.add(Box.createVerticalGlue());
-        JLabel welcome = new JLabel("Welcome to Weather App");
-        welcome.setAlignmentX(Component.CENTER_ALIGNMENT);
+    private void handleSearchButtonClicked() {
+        String city = userInputField.getText().trim();
+        ApiKey apiKey = new ApiKey();
+        final String key = apiKey.getApiKey();
+        WeatherApiConnector weatherApiConnector = new WeatherApiConnector();
+        transcript = weatherApiConnector.sendRequest(key, city);
 
-        greetings.add(welcome);
+        if (transcript.isCreated()) {
+            clearPreviousResults();
+            setCenterPanel();
+            updateSouthPanelMargin();
+            frame.pack();
+            frame.revalidate();
+            frame.repaint();
+        }
+    }
+
+    private void clearPreviousResults() {
+        details.removeAll();
+        imagePanel.removeAll();
+        centerPanel.removeAll();
+    }
+
+    private void setCenterPanel() {
+        centerPanel.setLayout(new GridLayout(0, 1));
+        addWeatherIcon();
+        addWeatherDetails();
+        frame.add(centerPanel, BorderLayout.CENTER);
+    }
+
+    private void addWeatherIcon() {
+        JLabel weatherIcon = loadImage(transcript.getCurrent().getCondition().getIcon());
+        imagePanel.setLayout(new GridLayout(0, 1, 0, 10));
+        imagePanel.add(weatherIcon);
+        centerPanel.add(imagePanel);
+    }
+
+    private void addWeatherDetails() {
+        details.setLayout(new GridLayout(0, 2, 0, 10));
+        details.setBorder(new EmptyBorder(0, 10, 10, 10));
+        JLabel cityTag = new JLabel("City:");
+        JLabel cityName = new JLabel(transcript.getLocation().getName());
+        JLabel temparatureTag = new JLabel("Temprature C" + DEGREE + ":");
+        JLabel temparature = new JLabel(transcript.getCurrent().getTemp_c() + DEGREE + "");
+        JLabel feelsLikeTag = new JLabel("Feels like:");
+        JLabel feelsLike = new JLabel(transcript.getCurrent().getFeelslike_c() + DEGREE + "");
+        JLabel conditionTag = new JLabel("Condition:");
+        JLabel condition = new JLabel(transcript.getCurrent().getCondition().getText());
+        JLabel windSpeedTag = new JLabel("Wind Speed (km/h):");
+        JLabel windSpeed = new JLabel(transcript.getCurrent().getWind_kph() + " km/h");
+        JLabel windDirectionTag = new JLabel("Wind Direction:");
+        JLabel windDirection;
+
+        // switch method to choose wind direction based on wind direction code
+        switch (transcript.getCurrent().getWind_dir()) {
+            case "N":
+                windDirection = new JLabel("North (N)");
+                break;
+            case "NNE":
+                windDirection = new JLabel("North-Northeast (NNE)");
+                break;
+            case "NE":
+                windDirection = new JLabel("Northeast (NE)");
+                break;
+            case "ENE":
+                windDirection = new JLabel("East-Northeast (ENE)");
+                break;
+            case "E":
+                windDirection = new JLabel("East (E)");
+                break;
+            case "ESE":
+                windDirection = new JLabel("East-Southeast (ESE)");
+                break;
+            case "SE":
+                windDirection = new JLabel("Southeast (SE)");
+                break;
+            case "SSE":
+                windDirection = new JLabel("South-Southeast (SSE)");
+                break;
+            case "S":
+                windDirection = new JLabel("South (S)");
+                break;
+            case "SSW":
+                windDirection = new JLabel("South-Southwest (SSW)");
+                break;
+            case "SW":
+                windDirection = new JLabel("Southwest (SW)");
+                break;
+            case "WSW":
+                windDirection = new JLabel("West-Southwest (WSW)");
+                break;
+            case "W":
+                windDirection = new JLabel("West (W)");
+                break;
+            case "WNW":
+                windDirection = new JLabel("West-Northwest (WNW)");
+                break;
+            case "NW":
+                windDirection = new JLabel("Northwest (NW)");
+                break;
+            case "NNW":
+                windDirection = new JLabel("North-Northwest (NNW)");
+                break;
+            default:
+                windDirection = new JLabel("Unknown");
+                break;
+        }
+
+        details.add(cityTag);
+        details.add(cityName);
+        details.add(temparatureTag);
+        details.add(temparature);
+        details.add(feelsLikeTag);
+        details.add(feelsLike);
+        details.add(conditionTag);
+        details.add(condition);
+        details.add(windSpeedTag);
+        details.add(windSpeed);
+        details.add(windDirectionTag);
+        details.add(windDirection);
+
+        centerPanel.add(details);
+    }
+
+    private void updateSouthPanelMargin() {
+        southPanel.setBorder(new EmptyBorder(20, 0, 0, 0));
+        frame.add(southPanel, BorderLayout.SOUTH);
+        frame.pack();
     }
 
     public void generateGui() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(greetings, BorderLayout.NORTH);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void setSouthPanel() {
+        southPanel.setBorder(new EmptyBorder(0, 0, 0, 0));
+        JLabel developerTag = new JLabel("Developed By:");
+        southPanel.add(developerTag);
+
+        JLabel developerName = new JLabel("Siraj");
+        developerName.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        developerName.setForeground(Color.BLUE);
+        developerName.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                openWebpage("https://sirajsaleem.com");
+            }
+        });
+        southPanel.add(developerName);
+        frame.add(southPanel, BorderLayout.SOUTH);
+    }
+
+    private void openWebpage(String website) {
+        try {
+            Desktop.getDesktop().browse(new URI(website));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // loadImage will fetch the image based on provided url
@@ -83,117 +260,6 @@ public class Gui implements ActionListener {
             return new JLabel(new ImageIcon(image));
         } catch (Exception e) {
             return new JLabel("Icon not found");
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String city = userInputField.getText().trim(); // Get the text from the input field
-        ApiKey apiKey = new ApiKey();
-        final String key = apiKey.getApiKey();
-        WeatherApiConnector weatherApiConnector = new WeatherApiConnector();
-        Transcript transcript = weatherApiConnector.sendRequest(key, city);
-
-        if (transcript.isCreated()) {
-            details.removeAll(); // to remove old result
-            imagePanel.removeAll(); // to remove old result
-            searchResult.removeAll(); // to remove old result
-
-            searchResult.setLayout(new GridLayout(0, 1));
-
-            JLabel weatherIcon = loadImage(transcript.getCurrent().getCondition().getIcon());
-            imagePanel.setLayout(new GridLayout(0, 1, 0, 10));
-            imagePanel.add(weatherIcon);
-            searchResult.add(imagePanel);
-
-            details.setLayout(new GridLayout(0, 2, 0, 10));
-            details.setBorder(new EmptyBorder(0, 10, 10, 10));
-            JLabel cityTag = new JLabel("City:");
-            JLabel cityName = new JLabel(transcript.getLocation().getName());
-            JLabel temparatureTag = new JLabel("Temprature C" + DEGREE + ":");
-            JLabel temparature = new JLabel(transcript.getCurrent().getTemp_c() + DEGREE + "");
-            JLabel feelsLikeTag = new JLabel("Feels like:");
-            JLabel feelsLike = new JLabel(transcript.getCurrent().getFeelslike_c() + DEGREE + "");
-            JLabel conditionTag = new JLabel("Condition:");
-            JLabel condition = new JLabel(transcript.getCurrent().getCondition().getText());
-            JLabel windSpeedTag = new JLabel("Wind Speed (km/h):");
-            JLabel windSpeed = new JLabel(transcript.getCurrent().getWind_kph() + " km/h");
-            JLabel windDirectionTag = new JLabel("Wind Direction:");
-            JLabel windDirection;
-
-            // switch method to choose wind direction based on wind direction code
-            switch (transcript.getCurrent().getWind_dir()) {
-                case "N":
-                    windDirection = new JLabel("North (N)");
-                    break;
-                case "NNE":
-                    windDirection = new JLabel("North-Northeast (NNE)");
-                    break;
-                case "NE":
-                    windDirection = new JLabel("Northeast (NE)");
-                    break;
-                case "ENE":
-                    windDirection = new JLabel("East-Northeast (ENE)");
-                    break;
-                case "E":
-                    windDirection = new JLabel("East (E)");
-                    break;
-                case "ESE":
-                    windDirection = new JLabel("East-Southeast (ESE)");
-                    break;
-                case "SE":
-                    windDirection = new JLabel("Southeast (SE)");
-                    break;
-                case "SSE":
-                    windDirection = new JLabel("South-Southeast (SSE)");
-                    break;
-                case "S":
-                    windDirection = new JLabel("South (S)");
-                    break;
-                case "SSW":
-                    windDirection = new JLabel("South-Southwest (SSW)");
-                    break;
-                case "SW":
-                    windDirection = new JLabel("Southwest (SW)");
-                    break;
-                case "WSW":
-                    windDirection = new JLabel("West-Southwest (WSW)");
-                    break;
-                case "W":
-                    windDirection = new JLabel("West (W)");
-                    break;
-                case "WNW":
-                    windDirection = new JLabel("West-Northwest (WNW)");
-                    break;
-                case "NW":
-                    windDirection = new JLabel("Northwest (NW)");
-                    break;
-                case "NNW":
-                    windDirection = new JLabel("North-Northwest (NNW)");
-                    break;
-                default:
-                    windDirection = new JLabel("Unknown");
-                    break;
-            }
-
-            details.add(cityTag);
-            details.add(cityName);
-            details.add(temparatureTag);
-            details.add(temparature);
-            details.add(feelsLikeTag);
-            details.add(feelsLike);
-            details.add(conditionTag);
-            details.add(condition);
-            details.add(windSpeedTag);
-            details.add(windSpeed);
-            details.add(windDirectionTag);
-            details.add(windDirection);
-
-            searchResult.add(details);
-            frame.add(searchResult, BorderLayout.CENTER);
-            frame.pack();
-            frame.revalidate();
-            frame.repaint();
         }
     }
 
